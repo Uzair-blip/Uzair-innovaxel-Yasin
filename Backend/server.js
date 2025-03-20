880e91bdc39afd4d9e258f1b49fece92d38c7296
uzairyasin <uzairyasin76@gmail.com>
2025-03-20 16:30:01 +0500

    Mongodb Setup done

---
+++ Backend/server.js
@@ -0,0 +1,15 @@
+import express from "express"
+import connectDB from "./config/db.js"
+import dotenv from "dotenv";
+// import urlRoutes from "./routes/url.routes.js"
+const app=express()
+const port =process.env.PORT
+app.use(express.json())
+dotenv.config();
+// app.use("/", urlRoutes);   // url routes 
+connectDB()
+app.listen(port || 5000, () => {
+    console.log(`Server is running on https://localhost:${port}`);
+  });
+  
+  
